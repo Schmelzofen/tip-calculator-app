021d2bd816f15dc43411c506b19537c6d5fdf56d
Schmelzofen <sertac.oezdemir@gmail.com>
2021-11-21 17:32:20 +0100

    mobile

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+let billInput = document.getElementById("billInput")
+let customTip = document.getElementById("customTip")
+let numberPeople = document.getElementById("numberPeople")
+let amountPerson = document.getElementById("amountPerPerson")
+let amountTotal = document.getElementById("amountTotal")
+let fuenf = document.getElementById("fuenf")
+let zehn = document.getElementById("zehn")
+let funfzehn = document.getElementById("funfzehn")
+let funfzwei = document.getElementById("funfzwei")
+let funfnull = document.getElementById("funfnull")
+
+resetCalc = () => {
+    console.log(fuenf.clicked)
+}
+
+console.log(fuenf.clicked)
